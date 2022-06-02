28c2b6f7cebdefa461a46dd2894e92e76957002c
Lucas Mariz <lucaspaulom@hotmail.com>
2022-06-01 21:40:00 -0300

    Adding trie structure

--- trie.py
+++ trie.py
@@ -1,3 +1,61 @@
+from typing import Union, Tuple
+
 class Trie:
-    def __init__(self, value=''):
+    def __init__(self, value='', numeric_value=None, parent=None, empty=True):
         self.value = value
+        self.numeric_value = numeric_value
+        self.parent = parent
+        self.empty = empty
+        self.children = {}
+
+    def _get_prefix(self, word):
+        res: Tuple[int, Union[str, None]] = (-1, None)
+        for key in self.children.keys():
+            last = len(key)
+            while last > 0:
+                if key[:last] == word[:last]:
+                    return (last, key)
+                last -= 1
+        return res
+
+    def contains(self, word):
+        if word == '' and '' in self.children.keys():
+            return self.children[''].numeric_value
+        if word == self.value:
+            if self.numeric_value != None:
+                return self.numeric_value
+
+        index, key = self._get_prefix(word)
+
+        if key == None:
+            return -1
+        return self.children[key].contains(word[index:])
+
+    def add(self, word, value):
+        self._add(word, value)
+
+    def _add_node(self, value, node):
+        if (node == None):
+            node = Trie()
+        node.parent = self
+        node.value = value
+        self.children[value] = node
+
+    def _add(self, word, value):
+        index, key = self._get_prefix(word)
+
+        if key == None:
+            self._add_node(word, Trie(empty=False, numeric_value=value))
+            if not self.empty and self.numeric_value != 0:
+                self._add_node('', Trie(empty=False, numeric_value=self.numeric_value))
+                self.numeric_value = None
+                self.empty = True   
+        elif index == len(key):
+            self.children[key].add(word[index:], value)
+        else:
+            index = word[:index]
+            temp = self.children.pop(key)
+            temp.value = key[index:]
+            self._add_node(index, None)
+            self.children[index]._add_node(word[index:], Trie(empty=False, numeric_value=value))
+            self.children[index]._add_node(key[index:], temp)
